b3cf838b5632d1c0546332a04ca469809b4143c4
Brent Gorton <brentgorton@users.noreply.github.com>
2017-03-20 16:03:13 -0600

    Update ant_sfdc.js

--- tasks/ant_sfdc.js
+++ tasks/ant_sfdc.js
@@ -141,7 +141,7 @@ module.exports = function(grunt) {
       rollbackOnError: true,
       useEnv: false,
       existingPackage: false,
-      testLevel : 'RunLocalTests'
+      testLevel : (this.data.options.runAllTests ? 'RunLocalTests' : 'NoTestRun')
     });
 
     grunt.log.writeln('Deploy Target -> ' + target);
